2139281a1d53a236b9664b73b09ac1a4a11d8b16
MohitR1999 <mohitr1999@gmail.com>
2020-10-21 22:30:27 +0530

    Home page created

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,22 @@
+import { Layout, Button } from 'antd';
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import Main from '../Main'
+import { Link } from 'react-router-dom';
+import styles from './index.module.css';
+
 const Home = () => {
     return (
-            <Link to="/analytics">
-                GO
-            </Link>
+        <Layout className={styles.hero}>
+            <Layout.Content>
+                <h1>Welcome to Awsar</h1>
+                <h2>We provide one stop solution for all your job needs</h2>
+                
+                <Button type="primary" size="large">
+                    <Link to="/analytics">
+                        GO
+                    </Link>
+                </Button>
+            </Layout.Content>
+        </Layout>
     )
 }
 
