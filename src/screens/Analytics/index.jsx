2e1fde5fec16925f375a7f8b03461f7a7e3ec8cc
MohitR1999 <mohitr1999@gmail.com>
2020-10-27 21:38:22 +0530

    ChartJS integrated, now data needed for visualization

--- src/screens/Analytics/index.jsx
+++ src/screens/Analytics/index.jsx
@@ -1,23 +1,66 @@
 import React from 'react';
-import {Button, DatePicker, version} from 'antd';
-import {Line} from '@antv/g2plot';
+import { Layout } from 'antd';
+import { Bar } from 'react-chartjs-2';
 const Analytics = () => {
-    const data = [
-        { year: '1991', value: 3 },
-        { year: '1992', value: 4 },
-        { year: '1993', value: 3.5 },
-        { year: '1994', value: 5 },
-        { year: '1995', value: 4.9 },
-        { year: '1996', value: 6 },
-        { year: '1997', value: 7 },
-        { year: '1998', value: 9 },
-        { year: '1999', value: 13 },
-      ];
+
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132)',
+                    'rgba(54, 162, 235)',
+                    'rgba(255, 206, 86)',
+                    'rgba(75, 192, 192)',
+                    'rgba(153, 102, 255)',
+                    'rgba(255, 159, 64)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    }
+
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                        fontColor: '#fff'
+                    },
+                },
+            ],
+            xAxes: [
+                {
+                    ticks: {
+                        fontColor: '#fff'
+                    },
+                },
+            ],
+        },
+        legend: {
+            labels: {
+                fontColor: '#fff'
+            }
+        }
+    }
 
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Layout>
+            <Layout.Content>
+                    <Bar data={data} options={options} />
+            </Layout.Content>
+        </Layout>
     )
 }
 
